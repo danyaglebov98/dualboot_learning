ad4d98ace206cfbeef02d0b48906efd571850ab0
d.glebov <d.glebov@gmail.com>
2020-05-06 20:21:20 +0300

    users

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
-class Web::ApplicationController < ApplicationController
+class Web::ApplicationController <
+  include Concerns::AuthHelper
+  helper_method :current_user
 end
