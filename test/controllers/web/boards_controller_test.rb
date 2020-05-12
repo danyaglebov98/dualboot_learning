25ce03e7c4ebf5d65feafa6a4e42f7b8aebaaf1b
d.glebov <d.glebov@gmail.com>
2020-05-12 18:45:40 +0300

    board's authentication

--- test/controllers/web/boards_controller_test.rb
+++ test/controllers/web/boards_controller_test.rb
@@ -3,6 +3,11 @@
 require 'test_helper'
 
 class Web::BoardsControllerTest < ActionController::TestCase
+  setup do
+    user = create(:user)
+    sign_in user
+  end
+
   test 'should get new' do
     get :show
     assert_response :success
