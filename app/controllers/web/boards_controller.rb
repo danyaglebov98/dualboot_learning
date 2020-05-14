3bb8f50d2d6250e5563f02f294fb92073d6625e7
d.glebov <d.glebov@gmail.com>
2020-05-14 14:12:50 +0300

    run rubocop and add create and update blocks

--- app/controllers/web/boards_controller.rb
+++ app/controllers/web/boards_controller.rb
@@ -2,7 +2,7 @@
 
 class Web::BoardsController < Web::ApplicationController
   before_action :authenticate_user!
-  
+
   def show
   end
 end
--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -11,27 +11,4 @@ class Admin::UsersControllerTest < ActionController::TestCase
     get :show, params: { id: user.id }
     assert_response :success
   end
-
-  test 'should get index' do
-    get :index
-    assert_response :success
-  end
-
-  test "should get edit" do
-    user = create(:user)
-    get :edit, params: { id: user.id }
-    assert_response :success
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should delete destroy" do
-    user = create(:user)
-    delete :destroy, params: {id: user.id }
-    assert_response :redirect
-  end
- 
 end
