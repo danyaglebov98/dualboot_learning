20bfe04dbf1f0a95f0aa9a1d21874fc989bd5617
d.glebov <d.glebov@gmail.com>
2020-06-04 16:16:10 +0300

    fix error and yarn lint

--- app/javascript/components/EditPopup/components/Form/Form.js
+++ app/javascript/components/EditPopup/components/Form/Form.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { has } from 'ramda';
 import TextField from '@material-ui/core/TextField';
