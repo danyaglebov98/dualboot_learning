ff7efefb103084154065bebc390981eb08e7c26f
d.glebov <d.glebov@gmail.com>
2020-06-10 13:59:24 +0300

    fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,8 @@ require('@rails/ujs').start();
 require('@rails/activestorage').start();
 require('channels');
 
+import 'stylesheets/application';
 import 'material-design-lite/material.js';
-
 import WebpackerReact from 'webpacker-react';
 import TaskBoard from 'components/TaskBoard';
 
