c2d4c60e417c2fbccbb626b8b68e12f44440a7b7
d.glebov <d.glebov@gmail.com>
2020-06-25 11:33:34 +0300

    fix problems

--- app/jobs/send_task_update_notification_job.rb
+++ app/jobs/send_task_update_notification_job.rb
@@ -1,4 +1,5 @@
 class SendTaskUpdateNotificationJob < ApplicationJob
+
   sidekiq_options queue: :mailers
   sidekiq_throttle_as :mailer
   sidekiq_options lock: :until_and_while_executing, on_conflict: { client: :log, server: :reject }
@@ -8,4 +9,4 @@ class SendTaskUpdateNotificationJob < ApplicationJob
 
     UserMailer.with(user: task.author, task: task).task_updated.deliver_now
   end
-  end
+end
