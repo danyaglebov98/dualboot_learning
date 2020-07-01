4606379283f0545631176735518bbf79d34e112f
d.glebov <d.glebov@gmail.com>
2020-07-01 17:49:17 +0300

    fix

--- app/jobs/send_password_resets_notification_job.rb
+++ app/jobs/send_password_resets_notification_job.rb
@@ -1,5 +1,4 @@
 class SendPasswordResetsNotificationJob < ApplicationJob
-<<<<<<< HEAD
 
   sidekiq_options queue: :mailers
   sidekiq_throttle_as :mailer
@@ -9,17 +8,5 @@ class SendPasswordResetsNotificationJob < ApplicationJob
     user = User.find(user_id)
 
     UserMailer.with(user: user).reset_password.deliver_now
-=======
-  sidekiq_options queue: :mailers
-  sidekiq_throttle_as :mailer
-  sidekiq_options lock: :until_and_while_executing, on_conflict: { client: :log, server: :reject }
-
-  def perform(user_id)
-    user = User.find_by(id: user_id)
-    return if user.blank?
-
-    UserMailer.with(user: user).reset_password.deliver_now
-  end
->>>>>>> run rubocop
   end
 end
