13053965de56b95e6f1535434c91ff64e70d2007
d.glebov <d.glebov@gmail.com>
2020-07-01 12:00:10 +0300

    fix errors

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,11 +4,11 @@ require 'sidekiq/throttled/web'
 require 'sidekiq_unique_jobs/web'
 
 Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = { url: ENV['REDIS_URL'] }
 end
 
 Sidekiq.configure_client do |config|
-    config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = { url: ENV['REDIS_URL'] }
 end
 
 Sidekiq::Throttled.setup!
